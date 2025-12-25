6548306b91d1cb9e1cae524aac7e533c92cef2cb
John Xu <jxujohn@gmail.com>
2025-12-25 09:40:20 -0500

    Initial Commit

---
+++ Dex/DexApp.swift
@@ -0,0 +1,21 @@
+//
+//  DexApp.swift
+//  Dex
+//
+//  Created by John Xu on 12/25/25.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct DexApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
